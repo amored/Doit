442ec9bf7a6ec2aaad982c4300e58e19a80edbae
Antonio <amored@gmail.com>
2019-09-03 18:55:40 -0400

    Convert to MVC design pattern - Add Item Class Data Model

--- Doit/Controllers/DoItViewController.swift
+++ Doit/Controllers/DoItViewController.swift
@@ -10,19 +10,37 @@ import UIKit
 
 class DoItViewController: UITableViewController {
 
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy Demogorgon"
+        itemArray.append(newItem3)
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
             itemArray = items
         }
+        
     }
+    
+//        // Do any additional setup after loading the view.
+//
+//        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+//            itemArray = items
+//        }
+//    }
 
 
     //MARK - Tableview Datasource Methods
@@ -35,7 +53,25 @@ class DoItViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "DoItItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //Ternary Operator
+        // value = condition ? valueIfTrue : valueIfFalse
+        
+        //Final Refactored Version with Ternary Operator
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        // Refactored Version of Ternary Operation include == true statement
+//        cell.accessoryType = item.done == true ? .checkmark : .none
+//
+//        //Original If Else Statement Checking if Item was Done
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
         
         return cell
     }
@@ -45,18 +81,20 @@ class DoItViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        } else {
+//            itemArray[indexPath.row].done = false
+//        }
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+        tableView.reloadData()
+    
         tableView.deselectRow(at: indexPath, animated: true)
         
-        
     }
+    
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -66,7 +104,10 @@ class DoItViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Task", style: .default) { (action) in
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             
---
+++ Doit/Data Model/item.swift
@@ -0,0 +1,15 @@
+//
+//  item.swift
+//  Doit
+//
+//  Created by Antonio Moreda on 9/3/19.
+//  Copyright © 2019 Antonio Moreda. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
